30e8d5301eba722fb1656bf1113ffe1e3da260a1
Tyler Duckworth <tduckworth91@gmail.com>
2015-02-28 17:42:06 -0700

    Groundwork for lab 5

--- application/config/routes.php
+++ application/config/routes.php
@@ -38,7 +38,7 @@
 |
 */
 
-$route['default_controller'] = 'lab3';
+$route['default_controller'] = 'lab5';
 $route['404_override'] = '';
 
 
---
+++ application/views/view_messages.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	
+	<title>tduck - Lab 5</title>
+	<link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="http://getbootstrap.com/2.3.2/assets/css/bootstrap-responsive.css" rel="stylesheet">
+
+</head>
+<body>
+	<div class="container">
+
+	<h3>tduck - Lab 5</h3>
+
+	<h5>Users</h5>
+
+</body>
+</html>
